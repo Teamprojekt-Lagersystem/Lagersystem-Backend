4f3596517ea72c62b162f3cbc537279eaa8c7dce
Neoyx <noah.leuft@gmail.com>
2024-11-04 12:19:17 +0100

    Implemented DepotRepository and its postgres implementation

--- src/main/kotlin/io/github/lagersystembackend/depot/DepotRepository.kt
+++ src/main/kotlin/io/github/lagersystembackend/depot/DepotRepository.kt
@@ -1,4 +1,9 @@
 package io.github.lagersystembackend.depot
 
 interface DepotRepository {
+    fun createDepot(name: String, description: String): Depot
+    fun getDepot(id: String): Depot?
+    fun getDepots(): List<Depot>
+    fun updateDepot(id: String, name: String?, description: String?): Depot?
+    fun deleteDepot(id: String): Boolean
 }
